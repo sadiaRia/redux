1d7a0f099fa6c91fe3983fbded425987e5324143
sadia <sadia@techhivebd.com>
2020-04-12 23:33:23 +0600

    add another action

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 const BUY_Cake = 'BUY_CAKE';
-
+const BUY_IceCream = "BUY_ICECREAM"
 //action
 function buyCake() {
   return {
@@ -10,11 +10,20 @@ function buyCake() {
   }
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_IceCream,
+    info: 'First Redux Action'
+  }
+}
+
+
 //reducers 
 //(prevState,action) => newState
 
 const initialState = {
-  numOfCake: 10
+  numOfCake: 10,
+  numOfIceCream: 10
 };
 
 const reducers = (state = initialState, action) => {
@@ -23,15 +32,22 @@ const reducers = (state = initialState, action) => {
       ...state,
       numOfCake: state.numOfCake - 1
     }
+    case BUY_ICECREAM: return {
+      ...state,
+      numOfCake: state.numOfIceCream - 1
+    }
+
     default: return state
   }
 }
 
 //store
 const store = createStore(reducers)
-console.log('initial State',store.getState());
-const unsubscribe = store.subscribe(()=>console.log('updated State ', store.getState()));
+console.log('initial State', store.getState());
+const unsubscribe = store.subscribe(() => console.log('updated State ', store.getState()));
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe()
